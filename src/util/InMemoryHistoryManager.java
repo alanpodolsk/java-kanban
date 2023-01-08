2b55922f47cbe04b653dc8dec898ad147b8d8f48
alanpodolsk <alanpodolsk@yandex.ru>
2023-01-09 02:33:25 +0300

    v2.1 Метод GetHistory есть в интерфейсах HistoryManager и TaskManager, в классах InMemoryHistoryManager и InMemoryTaskManager. Контроль длины списка (не более 10) реализован в методе InMemoryHistoryManager.add

--- src/util/InMemoryHistoryManager.java
+++ src/util/InMemoryHistoryManager.java
@@ -7,17 +7,21 @@ import java.util.List;
 
 public class InMemoryHistoryManager implements HistoryManager{
 
+    public static final byte MAX_SIZE = 10;
+
     List<Task> history = new ArrayList<>();
+
+    @Override
+    public List<Task> getHistory(){
+        return history;
+    }  // метод возвращает последние 10 просмотренных записей
 
     @Override
     public void add (Task task){
         history.add(task);
-        if(history.size()>10){
+        if(history.size()>MAX_SIZE){
             history.remove(0);
         }
     }
-    @Override
-    public List<Task> getHistory(){
-        return history;
-    }
+
 }
